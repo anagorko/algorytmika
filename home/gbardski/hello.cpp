756d9b491ed8dcb629fd49e16205d39e930b8b35
gbardski <grzegorz97@yahoo.com>
2014-09-16 09:59:43 +0200

    Mój Program

---
+++ home/gbardski/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << endl << endl << endl << endl;
+	cout << "YOLO !" << "\t" << "\t" << "xD" << endl;
+	cout << endl << endl << endl;
+
+	return 0;
+}
