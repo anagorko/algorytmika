929c2ddfbb32d79ff98b10acba9b6cb566ed7c3b
evildevill <adam.kordel@gmail.com>
2014-09-17 15:59:21 +0200

    kwadrat

---
+++ home/evildevill/kwadrat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+float a;
+ cout << "Podaj długość boku kwadratu\n";
+ cin >> a;
+ cout << "Pole kwadratu wynosi " << ( a * a ) << "\n";
+ cout << "Obwód kwadratu wynosi " << ( 4 * a ) << "\n";
+ 
+ return 0;
+}
+
