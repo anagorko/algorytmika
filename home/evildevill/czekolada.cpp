79162251b357065e5d98e99d04f80e2c952b9bc2
evildevill <adam.kordel@gmail.com>
2014-09-18 18:24:10 +0200

    czekolada

---
+++ home/evildevill/czekolada.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+ int a ,b , c, d ;
+ 
+ cout << "Podaj wymiary czekolad Zuzi i Antka\n";
+ cin >> a >> b >> c >> d ;
+ cout << "Łączna liczba kostek czekolady wynosi " << ((a*b)+(c*d)) << "\n";
+
+ return 0;
+
+}
