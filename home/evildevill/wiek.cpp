2c7d9dca4261d5c1183ecf958e002eccf4cede2f
evildevill <adam.kordel@gmail.com>
2014-09-22 19:45:26 +0200

    wiek

---
+++ home/evildevill/wiek.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ int wiek;
+
+
+ cout << "Podaj swój wiek\n";
+ cin >> wiek ;
+
+ if (wiek >= 18)
+
+  {
+
+   cout << "Jesteś pełnoletni\n";
+  
+  }
+
+  else
+
+  {
+
+   cout << "Jesteś niepełnoletni\n";
+
+  }
+
+ return 0;
+
+}
