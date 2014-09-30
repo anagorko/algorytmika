866fae7ee922c11562ac1cbc5761b8e0d3e82c97
franek <franek@witkowscy.net>
2014-09-30 22:15:10 +0200

    Która z liczb jest większa?

---
+++ fwitkowski/wzrostu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Podaj dwie liczby.\n";
+
+	int a,b;
+	cin >> a;
+	cin >> b;
+
+	if ( a <= b ){
+	cout << b << "\n";
+	}
+
+	else{
+	cout << a << "\n";
+	}
+
+	return 0;
+}
