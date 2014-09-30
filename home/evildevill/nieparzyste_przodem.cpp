c72f26e94011265b0bdf4f563dd3b5f1a4642da5
anagorko <amn@mimuw.edu.pl>
2014-09-30 09:15:49 +0200

    porzadek

---
+++ home/evildevill/nieparzyste_przodem.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ int a = 1, n;
+    
+  
+   while (a < 100);
+    {
+     n = (a % 2);
+     if (n = 0)
+      {
+       cout << a << " ";
+       a++;
+      }    
+      else 
+      {
+       cout << " ";
+       a++;
+      }
+     
+    }
+ 
+
+
+
+
+
+ 
+return 0;
+
+}
