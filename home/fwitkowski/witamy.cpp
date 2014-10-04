11233850babc67d2a58f9ca317d69fb128f783b8
franek <franek@witkowscy.net>
2014-10-04 19:18:09 +0200

    nie wiem po co, ale jest.

---
+++ home/fwitkowski/witamy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+	cout << "podaj liczbę n:\n";
+	int n;
+	cin >> n;
+
+	int i;
+	for (i = 1; i <= n; i++){	
+	cout << "witamy na pokładzie!\n";
+	}
+
+
+
+
+	return 0;
+}
