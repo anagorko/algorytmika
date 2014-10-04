3747658a2b7d2f73d578976f3e9dd878f50c4cd8
franek <franek@witkowscy.net>
2014-10-04 23:19:17 +0200

    poprawione

--- home/fwitkowski/nieparzyste_przodem.cpp
+++ home/fwitkowski/nieparzyste_przodem.cpp
@@ -7,10 +7,17 @@ int main ()
 	int i;
 	for ( i = 1; i <= 100; i++ ){
 		if ( i % 2 == 1 ){
-		cout << i << "\n";
+		cout << i << " ";
 		}
 	}
 
-
+	cout << "\n";
+	for ( i = 0; i <= 100; i++ ){
+		if ( i % 2 == 0 ){
+		cout << i << " ";
+		}
+	}
+	cout << "\n";
+	
 	return 0;
 }
