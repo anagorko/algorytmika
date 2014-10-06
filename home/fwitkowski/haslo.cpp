006e1c55424131e2f34c4049dc595450ce6bad28
franek <franek@witkowscy.net>
2014-10-06 22:10:11 +0200

    poprawione - pyta do skotku

--- home/fwitkowski/haslo.cpp
+++ home/fwitkowski/haslo.cpp
@@ -8,19 +8,22 @@ int main ()
 {
 	char string1[] = "mellon";
 	char string2[100];
+	int flaga = 0;
 	
-	cout << "podaj hasło:\n";
+	do{
+		cout << "podaj hasło:\n";
+		cin >> string2;
 
-	cin >> string2;
-
-	if ( strcmp(string2, string1) == 0){
-	printf ( "hasło jest prawidłowe.\n" );
+		if ( strcmp(string2, string1) == 0){
+		printf ( "hasło jest prawidłowe.\n" );
+		flaga = 1;
+		}
+		
+		else{	
+		printf ( "Hasło błędne\n" );
+		}
 	}
-	else{	
-	printf ( "Hasło błędne\n" );
-	}
-
-
+	while (flaga != 1);
 	
 	
 	return 0;
