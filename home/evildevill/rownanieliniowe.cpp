82701d0e0fb9e05b833e3261aedd3bac1174050a
evildevill <adam.kordel@gmail.com>
2014-11-02 22:40:19 +0100

    na 10000000% zle trymestr zadanie 2

---
+++ home/evildevill/rownanieliniowe.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int a,b,x;
+	
+	cin >> a;
+	cin >> b;
+	cout << a;
+	cout << b;
+
+	for (int i=0, i<=a,i++)
+		{
+			if (a%i=0)
+				{
+					x[i] = i;
+				}
+		}
+
+		if (sizeof x > 2)
+			{
+				cout << "NIESKOŃCZONOŚĆ\n";
+			}
+			else
+			{
+				cout << sizeof x << "\n";
+			}
+
+return 0;
+
+}
