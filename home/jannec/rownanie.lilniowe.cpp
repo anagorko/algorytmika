78877a48a59e265c267fb61ac206889418b7aea6
Adam Hornung <jan.maciejewski59@gmail.com>
2014-11-08 12:43:02 +0100

    HA HA! Trzaskam zadania jedno po drugim i DZIALAJA

---
+++ home/jannec/rownanie.lilniowe.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+int a,b;
+cout << "Podaj liczbe a: ";
+cin >> a;
+cout << "Podaj liczbe b: ";
+cin >> b;
+
+int x;
+
+	if(a==0, b== 0){ 
+		cout << "NIESKONCZONOSC rozwiazan\n";
+		
+		}else if(b%a==0){
+			cout << "Jedno rozwiazanie\n";
+			}else{
+				cout << "Nie ma rozwiazan\n";
+				}
+
+cout << x;
+
+
+
+
+return 0;
+}
