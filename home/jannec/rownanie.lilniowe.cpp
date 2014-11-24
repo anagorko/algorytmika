a5df90a2c8a26d93f7e44f982a24e606904470a7
Andrzej Nagórko <amn@mimuw.edu.pl>
2014-11-24 15:34:01 +0100

    Poprawione rozwiazanie zadania rownanie liniowe

--- home/jannec/rownanie.lilniowe.cpp
+++ home/jannec/rownanie.lilniowe.cpp
@@ -1,30 +1,26 @@
 #include<iostream>
 using namespace std;
 int main()
-
 {
+    int a,b;
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
 
-int a,b;
-cout << "Podaj liczbe a: ";
-cin >> a;
-cout << "Podaj liczbe b: ";
-cin >> b;
-
-int x;
-
-	if(a==0, b== 0){ 
-		cout << "NIESKONCZONOSC rozwiazan\n";
-		
-		}else if(b%a==0){
+	if(a == 0) {
+	    if (b == 0) {
+    		cout << "NIESKONCZONOSC rozwiazan\n";		
+	    } else {
+	        cout << "Nie ma rozwiazan\n";
+	    }
+	} else {
+	    if (b % a == 0) {
 			cout << "Jedno rozwiazanie\n";
-			}else{
-				cout << "Nie ma rozwiazan\n";
-				}
+		} else {
+			cout << "Nie ma rozwiazan\n";
+		}
+    }
 
-cout << x;
-
-
-
-
-return 0;
+    return 0;
 }
