cf3952a4327bda7b08f45fcd3a74278ade028bea
franek <franek@witkowscy.net>
2015-03-14 23:49:27 +0100

    czemu 6???

---
+++ home/fwitkowski/POM.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int l=0, a=0, p=0;
+	
+	
+	while(a!=-1){
+	cin >> a;
+		if(a==-1){
+			cout << l;
+		}else{
+			if(a>p){
+				l++;
+			}else{
+				p = a;
+			}
+		}	
+	}
+	
+return 0;
+}	
