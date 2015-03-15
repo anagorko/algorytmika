dc0b0f0fe5bab9564484687bf19ab06481c3cd56
franek <franek@witkowscy.net>
2015-03-15 14:18:34 +0100

    ...

---
+++ home/fwitkowski/SIL.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+	int n, m=1;
+	cin >> n;
+
+	for(int i=1; i<=n; i++){
+		 m=m*i;
+	cout << m << endl;
+	cout << i << endl;
+	}
+	
+	cout << m;
+
+
+
+
+
+
+
+	return 0;
+}
