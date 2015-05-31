5f8ade54546d687dc6dc549c863ec70e70b886ee
Adam Hornung <jan.maciejewski59@gmail.com>
2015-05-31 11:43:48 +0200

    teraz to juz naprawde nie ogarniam dlaczego nie chce sie skompilowac

--- home/jannec/pierwiastek_wielomianu.cpp
+++ home/jannec/pierwiastek_wielomianu.cpp
@@ -6,15 +6,17 @@ long long int power3(int x, int n)
 		return 1;
 	}else if(n%2==0){
 		double tmp = power3(x,n/2);
-		l3++;
+		
 		return tmp*tmp;
 	}else if(n%2==1){
 		double tmp = power3(x,n/2);
-		l3++;
-		l3++;
+		
 		return x*tmp*tmp;
 	}
 }
+
+
+
 int main()
 {
 
@@ -22,11 +24,10 @@ int r;
 cin >> r;
 int w=0;
 int e[r];
-while(w=<r){
+while(w<=r){
 	cin >> e[w];
 	w++;
 	}
-
 
 
 int a=e[r-1], s=e[0];
@@ -34,47 +35,64 @@ int p[1000000];
 int q[1000000];
 int g=0;
 
+w=a;
+
 for(w; w>0; w=w-1){
 	if(a%w==0){
-		p[g]=a;
+		p[g]=w;
 		g++;
 		}
 	}
+int t=g-1;
+
 g=0;
 
 for(int h=0; h<s; h++){
 	if(s%h==0){
-		q[g]=s;
+		q[g]=h;
 		g++;
 		}
 	}
-int z=p.lenght();
-int x=q.lenght();		
+	
+int u=g-1;	
+	
+int z=t;
+int x=u;		
 
 int c=2*(z+x);
 	
-//int pp[c];
+
 g=0;
 
-//for(int h=0; h<x ;h++){
-//	for(int j=0; j<z; j++){
-//		pp[g]=q[j]/p[h];
-//		}
-//}	
-string o;
 
-for(int h=r; h>0; h=h-1){
-	o[g]=h*power3(q[z], h)*power3(p[x], g);
-	g++;
-	}  
+int o[100000];
+for(int re=0; re<t; re++){
+	for(int er=0; er<u; er++){
+		for(int uu = 0; uu<1; uu++) 	
+			for(int h=r; h>0; h=h-1){
+				o[g]=h*power3(q[x], h)*power3(p[z], g);
+				g++;
+				}  
+		
+			g=0;
+		
+			int suma;
+			for(int y=0; y<r; y++){
+				suma=suma+o[g];
+				g++;
+				}
+			g=0;
+			if(suma==0){
+				cout << q << "/" << p << " ";
+				}
+			q[z]=(-1)*q[z];	
+			}
+		x=x-1;
+		}
+	z=z-1;
+	}
 
-int i=0;
-
-for(){
-	i=i
-
-
-
+cout << endl;
 
 return 0;
 }
