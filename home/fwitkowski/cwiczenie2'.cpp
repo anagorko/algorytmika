cc6aa50cee91dc13c94e159e5da63e34d6bd2bb7
Franek79 <franek@witkowscy.net>
2015-10-30 22:36:06 +0100

    wersja poprawiona cwiczenia 2.

---
+++ home/fwitkowski/cwiczenie2'.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=0;
+int b=0;
+do{
+cout << "podaj liczbe"<< endl;
+cin >> a;
+b=b+a;
+cout << b << endl;
+}while(a!=0);
+
+
+return 0;
+}
