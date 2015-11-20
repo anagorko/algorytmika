fc2e7e2ca74dba225eff550a96d307ff940f7eb9
jannec <grzegorz97@yahoo.com>
2015-11-20 12:38:02 +0100

    praca na lekcji

---
+++ home/jannec/na_lekcji_praca.cpp
@@ -0,0 +1,14 @@
+int solution(vector<int> &A) {
+    
+    int poczatek;
+    float suma, sumap;
+    
+    for(int q = 0; q < A.size(); q++){
+        
+        
+    
+        
+        
+    }
+    
+}
